a96930eda4afaf66bb76a64709b2a291cc7cbc23
skarab42 <skarab42@users.noreply.github.com>
2020-07-19 06:31:23 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+    node: true
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+    "no-console": "warn",
+    "no-debugger": "warn",
+    indent: ["error", 2],
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+    "linebreak-style": ["error", "unix"]
+  }
+};
