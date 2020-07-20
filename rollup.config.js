824c80651134351b423c9598b9b9525dd3e6ea51
skarab42 <skarab42@users.noreply.github.com>
2020-07-20 09:14:26 +0200

    preload...

--- rollup.config.js
+++ rollup.config.js
@@ -2,4 +2,4 @@ import mainConfig from "./rollup.config/main.js";
 import preloadConfig from "./rollup.config/preload.js";
 import rendererConfig from "./rollup.config/renderer.js";
 
-export default [mainConfig, preloadConfig, rendererConfig];
+export default [mainConfig, ...preloadConfig, rendererConfig];
--- rollup.config/preload.js
+++ rollup.config/preload.js
@@ -1,4 +1,4 @@
-import multiEntry from "@rollup/plugin-multi-entry";
+// import multiEntry from "@rollup/plugin-multi-entry";
 import path from "path";
 
 import { appDir, distDir } from "./common";
@@ -6,8 +6,13 @@ import { appDir, distDir } from "./common";
 const inputDir = path.resolve(appDir, "main");
 const outputDir = path.resolve(distDir, "main");
 
-export default {
-  input: `app/main/main-window/preload.js`,
-  output: { dir: outputDir },
-  plugins: [multiEntry()]
-};
+export default [
+  {
+    input: `${inputDir}/main-window/preload.js`,
+    output: {
+      format: "cjs",
+      sourcemap: true,
+      file: `${outputDir}/main-window/preload.js`
+    }
+  }
+];
