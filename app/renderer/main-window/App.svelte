0005cfda35851baeba3c87c6a2589a153211b981
skarab42 <skarab42@users.noreply.github.com>
2020-07-28 17:02:27 +0200

    clean landing page

--- app/renderer/main-window/App.svelte
+++ app/renderer/main-window/App.svelte
@@ -1,42 +1,5 @@
 <script>
-	let user = null;
-	let loading = false;
-	let darkMode = true;
-
-	remote.isDarkMode().then(r => darkMode = r);
-
-	$: remote.setDarkMode(darkMode);
-
-	function openTwitchChat() {
-	  remote.openTwitchChat();
-	}
-
-	function openTwitchLogin() {
-	  if (user) return;
-
-	  loading = true;
-
-	  remote.twitch("helix.users.getMe", true).then(res => {
-	    if (res.data) {
-	      user = res.data;
-	    }
-	    loading = false;
-	  });
-	}
-
-	openTwitchLogin();
+  import Landing from "./components/Landing.svelte";
 </script>
 
-{#if loading}
-	<h1>loading...</h1>
-{:else if user}
-<h1>Hello {user.display_name}</h1>
-	<button on:click={openTwitchChat}>Open chat window</button>
-{:else}
-	<h1>Please login with your Twitch account.</h1>
-	<button on:click={openTwitchLogin}>Login to Twitch</button>
-{/if}
-
-<label>
-	<input type="checkbox" bind:checked={darkMode}> Dark mode ({darkMode})
-</label>
+<Landing />
