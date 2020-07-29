35e1874bafdc9743e1cd170f92a637b7227e283a
skarab42 <skarab42@users.noreply.github.com>
2020-07-29 08:09:10 +0200

    sizing/colors

--- app/renderer/main-window/components/Landing.svelte
+++ app/renderer/main-window/components/Landing.svelte
@@ -14,15 +14,15 @@
 
 <div class="flex items-center justify-center h-screen overflow-auto">
   <div class="flex flex-col items-center">
-    <LogoSVG width="142" />
-    <div class="flex flex-col items-center">
+    <LogoSVG width="142" class="fill-current text-pink-900" />
+    <div class="flex flex-col items-center text-gray-300">
       <h1 class="text-5xl">Hello You !</h1>
       <p class="mb-5">Welcome home, please login with your Twitch account.</p>
       <button
-        class="flex items-center justify-center p-2 text-3xl bg-purple-500 rounded"
+        class="flex items-center justify-center px-4 py-2 text-3xl bg-pink-900 rounded"
         on:click={login}
       >
-        <TwitchSVG width="42" class="mr-2" /> Connect with Twitch.tv
+        <TwitchSVG width="28" class="fill-current mr-2" /> Connect with Twitch.tv
       </button>
     </div>
   </div>
