7443cb1f20ce2602984560e2ec63d953bd67b22f
skarab42 <skarab42@users.noreply.github.com>
2020-07-29 17:32:37 +0200

    user drawer draft

--- app/renderer/main-window/App.svelte
+++ app/renderer/main-window/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import "./components/DarkMode.svelte";
   import Landing from "./components/Landing.svelte";
+  import UserDrawer from "./components/UserDrawer.svelte";
 
   let user = null;
   let loading = false;
@@ -22,4 +23,8 @@
 
 {#if !user}
 <Landing on:login={openTwitchLogin} />
+{/if}
+
+{#if user}
+<UserDrawer {user} />
 {/if}
---
+++ app/renderer/main-window/components/UserDrawer.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let user = null;
+
+    let hovering;
+
+    const enter = () => (hovering = true);
+    const leave = () => (hovering = false);
+
+    $: hoveringClass = hovering ? "h-screen" : "";
+</script>
+
+<div
+  class="absolute bg-gray-800 p-1 right-0 {hoveringClass}"
+  on:mouseenter={enter} on:mouseleave={leave}
+>
+  <div class="flex items-center justify-center">
+    <img
+      src="{user.profile_image_url}"
+      class="rounded-full h-8"
+      alt="{user.display_name}"
+    />
+    {#if hovering}
+    <span class="px-2">{user.display_name}</span>
+    {/if}
+  </div>
+</div>
