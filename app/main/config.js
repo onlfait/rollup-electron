747dfdc143b5386c3c285f0a086fd50ffcb878bd
skarab42 <skarab42@users.noreply.github.com>
2020-08-01 07:37:35 +0200

    centralized paths in config

--- app/main/config.js
+++ app/main/config.js
@@ -1,11 +1,22 @@
 const { appConfig } = require("../package");
+const { app } = require("electron");
+const path = require("path");
 
 const isDev = process.argv.includes("--dev");
 const isDebug = process.argv.includes("--debug");
 
+const appPath = app.getAppPath();
+const userPath = app.getPath("userData");
+const publicPath = path.resolve(userPath, "public");
+const staticPath = path.resolve(__dirname, "../static");
+
 module.exports = {
   isDev,
   isDebug,
+  appPath,
+  userPath,
+  publicPath,
+  staticPath,
   appName: appConfig.name,
   appIcon: appConfig.icon,
   hasDevTools: isDev || isDebug,
