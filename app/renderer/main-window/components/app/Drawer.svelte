e9ed77c89c5437eda90cd18ff3c92a2e60b7c1c9
Sébastien Mischler <skarab@onlfait.ch>
2020-08-02 11:09:10 +0200

    add svelte-icons

--- app/renderer/main-window/components/app/Drawer.svelte
+++ app/renderer/main-window/components/app/Drawer.svelte
@@ -1,26 +1,21 @@
 <script>
   import Openable from "../Openable.svelte";
   import DarkModeSwitch from "./DarkModeSwitch.svelte";
+  import MdMenu from 'svelte-icons/md/MdMenu.svelte'
 </script>
 
-<style>
-  .top {
-    top: var(--titlebar-height);
-  }
-</style>
-
-<Openable class="flex">
+<Openable class="flex w-6 h-6 ">
   <div class="cursor-pointer">
-    login picture...
+    <MdMenu />
   </div>
   <div
     slot="open"
     on:click|stopPropagation
-    class="absolute ml-8 top right-0 bottom-0 overflow-auto shadow-md bg-secondary"
+    style="top: var(--titlebar-height)"
+    class="absolute ml-8 right-0 bottom-0 overflow-auto shadow-md bg-secondary"
   >
     <div class="flex flex-col">
-      <span class="p-2">Logout</span>
-      <span class="p-2">Profil</span>
+      <span class="p-2">Login</span>
       <span class="p-2">Settings</span>
       <span class="p-2">Action 1</span>
       <span class="p-2">Action 2</span>
