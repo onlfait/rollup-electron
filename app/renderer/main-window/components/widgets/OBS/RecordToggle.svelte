34ab56d3c8a04cb5b783395380f51de9483b03ca
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-08 18:09:08 +0200

    add recording on/off widget

---
+++ app/renderer/main-window/components/widgets/OBS/RecordToggle.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { recording, send } from "../../../stores/obs";
+
+  function toggleRecord() {
+    send($recording ? "StopRecording" : "StartRecording");
+  }
+</script>
+
+<div
+  on:click={toggleRecord}
+  class="flex flex-col w-full h-full items-center cursor-pointer hover:bg-black hover:bg-opacity-25"
+>
+	<div class="flex p-2 font-bold text-xl h-full items-center">
+    <div>{$recording ? "Stop" : "Start"} recording</div>
+  </div>
+</div>
