93ff54f9dee19e82c402bf356d1adc48743b3bd9
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-16 09:53:56 +0200

    set windown unique

--- app/main/windows/main/create.js
+++ app/main/windows/main/create.js
@@ -3,6 +3,7 @@ const createWindow = require("../../app/window/create");
 module.exports = function createMainWindow(options = {}) {
   return createWindow({
     ...options,
+    unique: true,
     name: "main",
     preload: true,
     storeBounds: true,
