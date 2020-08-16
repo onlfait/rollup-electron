2d244c53fb409b38073822730ade9652a6aad291
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-16 08:16:57 +0200

    extends all default variants

--- tailwind/config.js
+++ tailwind/config.js
@@ -1,4 +1,30 @@
 const darkMode = require("tailwindcss-dark-mode");
+const defaultConfig = require("tailwindcss/defaultConfig");
+
+const { variants } = defaultConfig;
+
+const darkVariants = [
+  "hover",
+  "focus",
+  "active",
+  "disabled",
+  "group-hover",
+  "focus-within",
+  "even",
+  "odd",
+  "placeholder"
+];
+
+function mergeDarkVariantes(key) {
+  const input = variants[key];
+  const output = [...input, "dark"];
+  input.forEach(variant => {
+    if (darkVariants.includes(variant)) {
+      output.push(`dark-${variant}`);
+    }
+  });
+  return output;
+}
 
 module.exports = {
   purge: [],
@@ -7,9 +33,9 @@ module.exports = {
     extend: {}
   },
   variants: {
-    textColor: ["hover", "dark", "dark-hover"],
-    borderColor: ["hover", "dark", "dark-hover"],
-    backgroundColor: ["hover", "dark", "dark-hover"]
+    textColor: mergeDarkVariantes("textColor"),
+    borderColor: mergeDarkVariantes("borderColor"),
+    backgroundColor: mergeDarkVariantes("backgroundColor")
   },
   plugins: [darkMode()]
 };
