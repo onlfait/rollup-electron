a2700d0c4fa39b5db5306496d8fec3e894b83807
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-17 08:04:26 +0200

    return a promise

--- app/main/preload/index.js
+++ app/main/preload/index.js
@@ -1,26 +1,29 @@
 const { contextBridge } = require("electron");
 
 module.exports = names => {
-  const api = {};
-  const init = [];
+  return new Promise(resolve => {
+    const api = {};
+    const init = [];
 
-  names.forEach(name => {
-    let options = {};
-    if (typeof name !== "string") {
-      options = name.options || {};
-      name = name.name;
-    }
-    const bridge = require(`./modules/${name}`);
-    bridge.init && init.push([bridge.init, options]);
-    bridge.api && (api[name] = bridge.api);
-  });
+    names.forEach(name => {
+      let options = {};
+      if (typeof name !== "string") {
+        options = name.options || {};
+        name = name.name;
+      }
+      const bridge = require(`./modules/${name}`);
+      bridge.init && init.push([bridge.init, options]);
+      bridge.api && (api[name] = bridge.api);
+    });
 
-  contextBridge.exposeInMainWorld("app", api);
+    contextBridge.exposeInMainWorld("app", api);
 
-  document.addEventListener("DOMContentLoaded", async () => {
-    for (let i = 0; i < init.length; i++) {
-      const [func, options] = init[i];
-      await func(options);
-    }
+    document.addEventListener("DOMContentLoaded", async () => {
+      for (let i = 0; i < init.length; i++) {
+        const [func, options] = init[i];
+        await func(options);
+      }
+      resolve();
+    });
   });
 };
