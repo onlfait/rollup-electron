91b79d019d1c44c2f7503d61c1c7d80cb3d02b3d
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-17 14:03:45 +0200

    moved toggle darkmode demo

--- app/renderer/windows/main/index.js
+++ app/renderer/windows/main/index.js
@@ -1,7 +1,2 @@
 import App from "./App.svelte";
 new App({ target: document.body });
-
-// document.addEventListener("click", async () => {
-//   const darkMode = await app.darkMode.toggle();
-//   app.titlebar.setTitle(darkMode ? "Nuit" : "Jour");
-// });
