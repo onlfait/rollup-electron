27779145bf21b1a7c57d4175f4523a9e8b15400c
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-27 08:11:26 +0200

    icon size

--- app/renderer/windows/main/components/Button.svelte
+++ app/renderer/windows/main/components/Button.svelte
@@ -3,18 +3,20 @@
 
   export let cls = '';
   export { cls as class };
+
   export let dark = true;
   export let icon = null;
+  export let iconSize = null;
   export let hideLabel = null;
 
   let color = dark ? 'text-light' : 'text-dark';
 </script>
 
 <button
-  class="flex space-x-1 p-2 uppercase rounded {color} {cls}"
+  class="{cls} flex space-x-1 p-2 uppercase rounded {color}"
   {...$$restProps}
   on:click
 >
-  {#if icon}<Icon {icon} />{/if}
+  {#if icon}<Icon {icon} size={iconSize} />{/if}
   <slot />
 </button>
--- app/renderer/windows/main/components/Icon.svelte
+++ app/renderer/windows/main/components/Icon.svelte
@@ -1,10 +1,17 @@
 <script>
   export let icon = null;
-  export let cls = '';
+  export let w = 6;
+  export let h = 6;
+  export let size = null;
 
+  export let cls = '';
   export { cls as class };
+
+  if (size) {
+    w = h = size;
+  }
 </script>
 
-<span class="flex inline-block w-6 h-6 {cls}">
+<span class="flex inline-block w-{w} h-{h} {cls}">
   <svelte:component this={icon} />
 </span>
