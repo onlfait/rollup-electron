7df15eb2e49610f8249b80b72b9b59c8fcd00a29
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-08-30 13:45:51 +0200

    Update WidgetButton.svelte

--- app/renderer/windows/main/components/Widgets/WidgetButton.svelte
+++ app/renderer/windows/main/components/Widgets/WidgetButton.svelte
@@ -1,9 +1,16 @@
 <script>
   export let widget;
+  export let disabled = false;
 
-  let classList = "flex w-full h-full cursor-pointer items-center justify-center hover:bg-black hover:bg-opacity-25";
+  let classList = "absolute inset-0 flex w-full h-full cursor-pointer items-center justify-center hover:bg-black hover:bg-opacity-25";
 </script>
 
-<div id="widget-button-{widget.id}" class={classList} on:click>
+<div id="widget-button-{widget.id}" class="{classList} {disabled && 'pointer-events-none'}" on:click>
+  {#if disabled}
+  <div class="p-2 rounded text-center font-bold transform -rotate-45 bg-black opacity-50">
+    <slot name="disabled" />
+  </div>
+  {:else}
   <slot />
+  {/if}
 </div>
