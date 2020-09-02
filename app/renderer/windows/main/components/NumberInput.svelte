643dd92a7d797d8ffe6f1cdde2b09ca6f591db30
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-02 17:09:19 +0200

    Create NumberInput.svelte

---
+++ app/renderer/windows/main/components/NumberInput.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let label;
+  export let value;
+  export let min = 0;
+  export let step = 1;
+</script>
+
+<div class="flex flex-col">
+  <div class="font-medium">{label}</div>
+  <input
+    {min}
+    {step}
+    bind:value
+    {...$$props}
+    type="number"
+    class="p-2 flex-auto rounded w-20"
+  />
+</div>
