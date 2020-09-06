3b49a17e363cba6fb37babcd5d0ae3540769f3a0
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-06 06:20:17 +0200

    fix and move splitter component

--- app/renderer/windows/main/components/Splitter.svelte
+++ app/renderer/windows/main/components/Splitter.svelte
@@ -2,7 +2,9 @@
   export let flex = "row";
   export let min = 10;
   export let max = 90;
-  export let percent = 50;
+  export let sizes = [25, 75];
+  export let splitterSize = 4;
+  export let save = false;
 
   export let cls = "";
   export { cls as class };
@@ -10,10 +12,19 @@
   let dragging = false;
   let container = null;
 
+  let [aSize, bSize] = sizes;
+
+  if (save) {
+    const store = localStorage.getItem(`splitter.${save}`);
+    if (store) {
+      [aSize, bSize] = JSON.parse(store);
+    }
+  }
+
   let dimension = flex === "row" ? "width" : "height";
 
   let splitterCursor = flex === "row" ? "ew-resize" : "ns-resize";
-  let splitterStyle = `${dimension}:4px; cursor:${splitterCursor}`;
+  let splitterStyle = `${dimension}:${splitterSize}px; cursor:${splitterCursor}`;
   let splitterClass = "hover:bg-black hover:bg-opacity-25";
 
   function setPos(event) {
@@ -24,7 +35,11 @@
     const y = event.clientY - top;
     const pos = flex === "row" ? x : y;
     const len = flex === "row" ? w : h;
-    percent = Math.max(min, Math.min(max, pos / len * 100));
+    aSize = Math.max(min, Math.min(max, pos / len * 100));
+    bSize = 100 - aSize;
+    if (save) {
+      localStorage.setItem(`splitter.${save}`, JSON.stringify([aSize, bSize]));
+    }
   }
 
   function drag(node, callback) {
@@ -49,12 +64,12 @@
   }
 </script>
 
-<div bind:this={container} class="relative flex flex-{flex} h-full {cls}">
-  <div style="{dimension}: {percent}%;">
+<div bind:this={container} class="flex flex-{flex} h-full overflow-auto {cls}">
+  <div style="{dimension}:{aSize}%">
 		<slot name="a"></slot>
 	</div>
   <div use:drag={setPos} style={splitterStyle} class={splitterClass}></div>
-  <div class="flex-auto">
+  <div style="{dimension}:{bSize}%">
 		<slot name="b"></slot>
 	</div>
 	<slot />
