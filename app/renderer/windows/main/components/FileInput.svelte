9fdd461d69e1f90e43af1e32fbefebe9d72391b0
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-06 06:20:32 +0200

    add multiple prop

--- app/renderer/windows/main/components/FileInput.svelte
+++ app/renderer/windows/main/components/FileInput.svelte
@@ -5,12 +5,14 @@
   const dispatch = createEventDispatcher();
 
   export let label = "Select a file";
+  export let multiple = null;
   export let accept = "*";
 
   let input = "";
 
   function onChange(event) {
-    dispatch("file", event.target.files[0]);
+    const files = event.target.files;
+    dispatch("file", multiple ? files : files[0]);
     input.value = "";
   }
 </script>
@@ -19,6 +21,7 @@
   <Button {...$$props}>{label}</Button>
   <input
     {accept}
+    {multiple}
     type="file"
     bind:this={input}
     on:change="{onChange}"
