138086ff6c0f8d42d7af0e398cc8f2c912768238
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-08 19:44:06 +0200

    refactoring keyframes...

---
+++ app/renderer/windows/main/components/Anime/Keyframe.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let keyframe;
+  export let scale = 1;
+
+  $: x = keyframe.x * scale;
+</script>
+
+<div class="absolute top-0 bottom-0 flex items-center" style="left:{x}px">
+  <div class="w-5 h-5 transform rotate-45 bg-blue-600 bg-opacity-50"></div>
+</div>
--- app/renderer/windows/main/components/Anime/Keyframes.svelte
+++ app/renderer/windows/main/components/Anime/Keyframes.svelte
@@ -1,83 +1,29 @@
-<script context="module">
-	let currentKeyframe;
-</script>
-
 <script>
-  import { v4 as uuid } from "uuid";
-  import pannable from "./pannable.js";
-
-  import { createEventDispatcher } from "svelte";
-
-  const dispatch = createEventDispatcher();
-
-  export let cls = "";
-  export { cls as class };
+  import Keyframe from "./Keyframe.svelte";
 
   export let anime;
   export let x = 0;
   export let scale = 1;
 
-  let panning = false;
+  export let cls = "";
+  export { cls as class };
 
-  function selectKeyframe(keyframe) {
-    console.log("selectKeyframe:", keyframe);
-    dispatch("keyframe", keyframe);
-    currentKeyframe = keyframe;
-    anime = anime;
-  }
-
-  function clampDelay(delay) {
-    return Math.max(0, delay);
-  }
-
-  function addKeyframe(event) {
-    const bbox = event.currentTarget.getBoundingClientRect();
-    const delay = clampDelay((event.clientX - bbox.x - x - 6) * scale);
-    const keyframe = { id: uuid(), delay };
-    anime.keyframes = [ ...anime.keyframes, keyframe ];
-    selectKeyframe(keyframe);
-  }
-
-
-  function handlePanStart() {
-    panning = true;
-  }
-
-  function handlePanMove({ detail }) {
-    const delay = currentKeyframe.delay + detail.dx * scale;
-    currentKeyframe.delay = clampDelay(delay);
-    anime.keyframes = anime.keyframes;
-  }
-
-  function handlePanEnd() {
-    panning = false;
-  }
-
-  function keyframeClass(keyframe) {
-    const current = keyframe.id === currentKeyframe.id;
-    return current ? "bg-red-600" : "bg-blue-600";
-  }
+  anime.keyframes = [
+    { x: 0 },
+    { x: 10 },
+    { x: 25 },
+    { x: 75 },
+    { x: 155 },
+    { x: 255 },
+  ];
 </script>
 
-<div
-  on:dblclick={addKeyframe}
-  class="relative whitespace-no-wrap overflow-hidden {cls}"
->
-
-  <span class="absolute" style="transform:translateX({x}px)">
-    {#each anime.keyframes as keyframe}
-    <span
-      class="absolute"
-      use:pannable
-      on:panstart={handlePanStart}
-    	on:panmove={handlePanMove}
-    	on:panend={handlePanEnd}
-      on:mousedown|stopPropagation={selectKeyframe.bind(null, keyframe)}
-      style="transform:translate({keyframe.delay / scale}px, 6px)"
-    >
-      <div class="w-3 h-3 transform rotate-45 {keyframeClass(keyframe)} bg-opacity-50"></div>
-    </span>
-    {/each}
-  </span>
-
+<div class="px-2 overflow-hidden {cls}">
+	<div class="relative w-full h-full">
+    <div class="absolute top-0 bottom-0" style="left:{x}px">
+      {#each anime.keyframes as keyframe}
+      <Keyframe bind:keyframe {scale} />
+      {/each}
+    </div>
+	</div>
 </div>
