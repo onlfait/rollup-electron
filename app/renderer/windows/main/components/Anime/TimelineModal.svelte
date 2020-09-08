35fc3ac546d2a9fc30b340fb37cbccbab4b0d477
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-08 19:43:36 +0200

    removed dispatcher

--- app/renderer/windows/main/components/Anime/TimelineModal.svelte
+++ app/renderer/windows/main/components/Anime/TimelineModal.svelte
@@ -7,11 +7,11 @@
   import Splitter from "../Splitter.svelte";
   import MdArrowBack from "svelte-icons/md/MdArrowBack.svelte";
 
-  const dispatch = createEventDispatcher();
-
   export let timeline;
 
   let element;
+
+  const dispatch = createEventDispatcher();
 
   let position = "absolute z-50 left-0 right-0 bottom-0";
   let theme = "bg-light text-dark dark:bg-dark dark:text-light";
@@ -23,15 +23,13 @@
 
 <div bind:this={element} class="{position} {theme}" style="top:32px">
   <Splitter save="timeline.main" sizes={[75,25]} flex="col" class="relative">
-    <div slot="a">
-      <div class="p-2 flex items-center space-x-2">
+    <div slot="a" class="p-2 flex items-center space-x-2">
         <Button class="bg-primary" icon={MdArrowBack} on:click={dispatch.bind(null, "close")}>
           BACK
         </Button>
         <div>
           Objects: {timeline.length}
         </div>
-      </div>
     </div>
     <div slot="b" class="h-full">
       <Timeline bind:timeline />
