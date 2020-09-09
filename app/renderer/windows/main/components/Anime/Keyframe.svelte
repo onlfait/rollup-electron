df24f433aa9ded3c5201b3ccd52bf5043013412b
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-09 07:11:17 +0200

    select/move keyframe

--- app/renderer/windows/main/components/Anime/Keyframe.svelte
+++ app/renderer/windows/main/components/Anime/Keyframe.svelte
@@ -1,10 +1,28 @@
 <script>
+  import pannable from "../pannable.js";
+
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
   export let keyframe;
   export let scale = 1;
 
   $: x = keyframe.x * scale;
+
+  function onPanMove({ detail }) {
+    dispatch("move", detail);
+  }
 </script>
 
-<div class="absolute top-0 bottom-0 flex items-center" style="left:{x}px">
+<div
+  on:click
+  on:dblclick
+  use:pannable
+  on:panmove={onPanMove}
+  on:mousedown|stopPropagation
+  class="absolute top-0 bottom-0 flex items-center"
+  style="left:{x}px"
+>
   <div class="w-5 h-5 transform rotate-45 bg-blue-600 bg-opacity-50"></div>
 </div>
--- app/renderer/windows/main/components/Anime/Keyframes.svelte
+++ app/renderer/windows/main/components/Anime/Keyframes.svelte
@@ -16,13 +16,35 @@
     { x: 155 },
     { x: 255 },
   ];
+
+  function updateKeyframe(index, props) {
+    anime.keyframes = anime.keyframes.map((keyframe, i) => {
+      return i === index ? { ...keyframe, ...props } : keyframe;
+    });
+  }
+
+  function onMove(index, { detail }) {
+    const keyframe = anime.keyframes[index];
+    const x = Math.max(0, keyframe.x + detail.dx);
+    updateKeyframe(index, { x });
+  }
+
+  function onSelect(index) {
+    const keyframe = anime.keyframes[index];
+    console.log("select:", index, keyframe);
+  }
 </script>
 
 <div class="px-2 overflow-hidden {cls}">
 	<div class="relative w-full h-full">
     <div class="absolute top-0 bottom-0" style="left:{x}px">
-      {#each anime.keyframes as keyframe}
-      <Keyframe bind:keyframe {scale} />
+      {#each anime.keyframes as keyframe, i}
+      <Keyframe
+        {scale}
+        {keyframe}
+        on:move={onMove.bind(null, i)}
+        on:mousedown={onSelect.bind(null, i)}
+      />
       {/each}
     </div>
 	</div>
