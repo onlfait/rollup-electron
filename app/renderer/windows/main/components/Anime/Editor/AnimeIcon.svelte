0b6c8ede0c2ef7360fd2805bc7bc2d87aaaab045
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-23 06:34:37 +0200

    Create AnimeIcon.svelte

---
+++ app/renderer/windows/main/components/Anime/Editor/AnimeIcon.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Icon from "../../Icon.svelte";
+  import { getAnimeIcon } from "../libs/anime";
+
+  export let type;
+</script>
+
+<Icon icon={getAnimeIcon(type)} class="w-4 h-4 flex-shrink-0" />
