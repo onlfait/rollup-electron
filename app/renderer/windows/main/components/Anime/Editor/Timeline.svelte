bfaff527a4c21e08a94db1cc48568b41bde33d1a
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-23 16:14:35 +0200

    removable anime

--- app/renderer/windows/main/components/Anime/Editor/Timeline.svelte
+++ app/renderer/windows/main/components/Anime/Editor/Timeline.svelte
@@ -1,7 +1,10 @@
 <script>
+  import Icon from "../../Icon.svelte";
   import Grid from "./Timeline/Grid.svelte";
   import AnimeIcon from "./AnimeIcon.svelte";
   import { isSameAnime } from "../libs/anime";
+
+  import MdDeleteForever from "svelte-icons/md/MdDeleteForever.svelte";
 
   export let animes;
   export let timeline;
@@ -23,6 +26,13 @@
   function selectedClass(currentAnime, anime) {
     return isSameAnime(currentAnime, anime) ? "bg-black bg-opacity-25" : "";
   }
+
+  function deleteAnime(anime) {
+    if (isSameAnime(currentAnime, anime)) {
+      currentAnime = null;
+    }
+    animes = animes.filter(a => !isSameAnime(a, anime));
+  }
 </script>
 
 <Grid {...timeline} on:resize={onResize} on:state={onState}>
@@ -33,9 +43,12 @@
     Timeline...
   </div>
   {#each animes as anime}
-    <div class="p-2 flex space-x-2 {selectedClass(currentAnime, anime)}" on:click={selectAnime.bind(null, anime)}>
+    <div class="flex pl-2 items-center space-x-2 {selectedClass(currentAnime, anime)}" on:click={selectAnime.bind(null, anime)}>
       <AnimeIcon type={anime.type} />
-      <div class="truncate">{anime.filename}</div>
+      <div class="p-2 truncate flex-1">{anime.filename}</div>
+      <div class="p-2 cursor-pointer hover:bg-red-600" on:click|stopPropagation={deleteAnime.bind(null, anime)}>
+        <Icon icon={MdDeleteForever} />
+      </div>
     </div>
     <div class="p-2 truncate {selectedClass(currentAnime, anime)}" on:click={selectAnime.bind(null, anime)}>
       {anime.id}
