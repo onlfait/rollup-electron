1f68c389f9fa20ea47f94deca34b45c7aca15563
skarab42 <62928763+skarab42@users.noreply.github.com>
2020-09-23 06:38:28 +0200

    settings panel component

--- app/renderer/windows/main/components/Anime/Editor/Settings.svelte
+++ app/renderer/windows/main/components/Anime/Editor/Settings.svelte
@@ -1,21 +1,41 @@
 <script>
-  import Icon from "../../Icon.svelte";
-  import { getAnimeIcon } from "../libs/anime";
+  import AnimeIcon from "./AnimeIcon.svelte";
+  import Panel from "./Settings/Panel.svelte";
 
-  export let animes;
   export let currentAnime;
+
+  let infoEntries = [];
+  let attrsKeys = [];
+
+  $: infoEntries = currentAnime && Object.entries(currentAnime.info);
+  $: attrsKeys = currentAnime && Object.keys(currentAnime.attributes);
 </script>
 
 {#if currentAnime}
-<div class="p-2 flex space-x-2 bg-primary-dark">
-  <Icon icon={getAnimeIcon(currentAnime.type)} class="w-4 h-4 flex-shrink-0" />
-  <div class="truncate">{currentAnime.filename}</div>
-</div>
+  <div class="p-2 flex space-x-2 bg-primary-dark">
+    <AnimeIcon type={currentAnime.type} />
+    <div class="truncate">{currentAnime.filename}</div>
+  </div>
+  <Panel title="Info" visible={infoEntries.length}>
+    {#each infoEntries as [key, value]}
+    <div class="p-2 flex space-x-2 items-center">
+      <div class="flex-auto">{key}</div>
+      <div>{value}</div>
+    </div>
+    {/each}
+  </Panel>
+  <Panel title="Attributes" visible={attrsKeys.length}>
+    {#each attrsKeys as key}
+    <div class="p-2 flex space-x-2 items-center">
+      <div class="flex-auto">{key}</div>
+    </div>
+    {/each}
+  </Panel>
 {:else}
-<div class="p-2 truncate bg-primary-dark">
-  No files added/selected...
-</div>
-<div class="p-2">
-  Drag/Drop some file on the timeline ;)
-</div>
+  <div class="p-2 truncate bg-primary-dark">
+    No files added/selected...
+  </div>
+  <div class="p-2">
+    Drag/Drop some file on the timeline ;)
+  </div>
 {/if}
---
+++ app/renderer/windows/main/components/Anime/Editor/Settings/Panel.svelte
@@ -0,0 +1,30 @@
+<script>
+  import Icon from "../../../Icon.svelte";
+  import MdExpandMore from "svelte-icons/md/MdExpandMore.svelte";
+  import MdExpandLess from "svelte-icons/md/MdExpandLess.svelte";
+
+  export let title;
+  export let visible = true;
+  export let expended = true;
+
+  let divide = "divide-y divide-blue-600 divide-opacity-50";
+
+  function toggle() {
+    expended = !expended;
+  }
+
+</script>
+
+{#if visible}
+<div class="p-2 bg-primary flex space-x-2 cursor-pointer" on:click={toggle}>
+  <Icon icon={expended ? MdExpandLess : MdExpandMore} />
+  <div class="truncate">{ title }</div>
+</div>
+
+{#if expended}
+<div class="{divide}">
+  <slot />
+</div>
+{/if}
+
+{/if}
